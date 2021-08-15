9cffa0e02741317414679c6d00ef3e2c5352f99a
unknown <ankita.ithub@gmail.com>
2021-08-15 14:00:50 +0530

    grid scroll event

--- src/app/gridrouter/gridrouter.component.ts
+++ src/app/gridrouter/gridrouter.component.ts
@@ -1,26 +1,33 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gridrouter',
   templateUrl: './gridrouter.component.html',
   styleUrls: ['./gridrouter.component.css']
 })
-export class GridrouterComponent implements OnInit {
-  @HostListener('window:scroll', ['$event']) scrollHandler(event:any) {
-    console.debug("Scroll Event"+event);
+export class GridrouterComponent implements OnInit,OnDestroy {
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event: any) {
+    console.debug("Scroll Event");
   }
+  i:any=0
   items: any = []
   constructor() { }
 
   ngOnInit(): void {
-    for (var i = 1; i <= 4; i++) {
-      this.items.push(`Button '${i}' is clicked`);
+    for (this.i = 1; this.i <= 4; this.i++) {
+      this.items.push(`Button '${this.i}' is clicked`);
     }
+    window.addEventListener('scroll', this.scrollEvent, true); 
   }
-  push(d:any){
+  push(d: any) {
     alert(d)
   }
-  // scrollHandler(e:any){
-  //  console.log(e)
-  // }
+  scrollEvent = (event: any): void => {
+      this.items.push(`Button '${this.i}' is clicked`);
+      this.i++;
+  }
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scrollEvent, true);
+  }
 }
